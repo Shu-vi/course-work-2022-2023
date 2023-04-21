df6c265fdf25ac17bb595e2fab5f8a146a83b734
Vadim <vad.general@mail.ru>
2023-04-21 09:32:03 +0300

    Fix: перейти к жёсткой привязке количества пользователей

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -9,8 +9,8 @@ interface IInput {
 interface Props {
     inputs: IInput[];
     onInputChange: (input: IInput, value: string) => void;
-    onInputDelete: (input: IInput) => void;
-    onInputAdd: () => void;
+    // onInputDelete?: (input: IInput) => void;
+    // onInputAdd?: () => void;
     onCompareClick: () => void;
     error: string;
 }
@@ -18,16 +18,16 @@ interface Props {
 export const InputForm: FC<Props> = ({
                                          inputs,
                                          onInputChange,
-                                         onInputDelete,
-                                         onInputAdd,
+                                         // onInputDelete,
+                                         // onInputAdd,
                                          onCompareClick,
                                          error
                                      }) => {
     return (
         <Form>
             {inputs.map((input) => (
-                <Form.Group className="mb-3" controlId="formBasicEmail" key={input.id}>
                     <Form.Control
+                        key={input.id}
                         className="mb-2"
                         type="email"
                         placeholder="Введите адрес страницы... Например: id123456789"
@@ -36,22 +36,10 @@ export const InputForm: FC<Props> = ({
                             onInputChange(input, event.target.value);
                         }}
                     />
-                    {inputs.length > 2 && (
-                        <Button variant="danger" onClick={() => onInputDelete(input)}>
-                            Удалить
-                        </Button>)
-                    }
-
-                </Form.Group>
             ))}
             {inputs.length > 0 && (
                 <Button variant="success" className="mr-1" onClick={onCompareClick}>
                     Сравнить
-                </Button>
-            )}
-            {inputs.length < 5 && (
-                <Button variant="primary" onClick={onInputAdd}>
-                    Добавить ещё
                 </Button>
             )}
             <div className='text-danger'>{error}</div>
