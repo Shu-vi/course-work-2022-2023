d01885a908e5385ea42a7528a0c8a26a5bc26b41
Vadim <vad.general@mail.ru>
2023-04-21 09:29:20 +0300

    Fix: убрать лишние поля из представления

--- src/views/IViewUser.ts
+++ src/views/IViewUser.ts
@@ -13,11 +13,8 @@ export interface IViewUser {
     last_name: string;
     deactivated?: boolean;
     alcohol: number | undefined;
-    inspired_by: string | undefined;
     langs: string[] | undefined;
     life_main: number | undefined;
-    people_main: number | undefined;
-    political: number | undefined;
     religion: string | undefined;
     smoking: number | undefined;
 }
