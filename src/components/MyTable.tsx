c3e8a84589ba330ef9d9427b467c6423902bc2fe
Vadim <vad.general@mail.ru>
2023-05-05 14:33:18 +0300

    Refactor: удалить лишний импорт

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactNode} from "react";
+import React, {FC} from "react";
 import {Table} from "react-bootstrap";
 import {IViewUser} from "../views/IViewUser";
 import {IViewUniversity} from "../views/IViewUniversity";
